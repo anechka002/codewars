e6e39468c5fd8d9bb05a4a5cca1f19819aabfe16
Anna <blinova.anneta@gmail.com>
2024-06-26 12:58:58 +0200

    Generate range of integers

---
+++ 8_kyu/GenerateRangeOfIntegers.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+
+// Examples
+// (1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// (-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+// (1, 15, 20) -> [1]
+// Note
+// start < stop
+// step > 0
+
+// SOLUTION:
+
+function generateRange(min, max, step){
+  let res = [];
+  for(let i = min; i <= max; i+=step) {
+    res.push(i)
+  }
+  return res
+}
+console.log(generateRange(2, 10, 2))
