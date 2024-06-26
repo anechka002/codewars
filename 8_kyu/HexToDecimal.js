70d0a0d78644ae76d01ddbfcdff08eeb77121f14
Anna <blinova.anneta@gmail.com>
2024-06-26 13:13:00 +0200

    Hex to Decimal

---
+++ 8_kyu/HexToDecimal.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// SOLUTION:
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
+console.log(hexToDec('FF'))
