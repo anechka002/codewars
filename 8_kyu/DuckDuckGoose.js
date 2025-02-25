e8875f43192aa70a4cb5280b77ada84d75eac4c9
Anna <blinova.anneta@gmail.com>
2025-02-25 09:01:48 +0100

    Duck Duck Goose

---
+++ 8_kyu/DuckDuckGoose.js
@@ -0,0 +1,21 @@
+// Description:
+
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+// Task:
+
+// Given an array of Player objects and a position (first position is 1), return the name of the chosen Player.
+// name is a property of Player objects, e.g Player.name
+
+// Example:
+
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+
+// Solution:
+
+duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name
+
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 11))
+console.log(duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 10))
