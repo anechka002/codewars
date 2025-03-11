dbb469ef0367d2875067e4becbad6bd1075dd018
Anna <blinova.anneta@gmail.com>
2025-03-11 14:44:49 +0100

    get ascii value of character

---
+++ 7_kyu/GetAsciiValueOfCharacter.js
@@ -0,0 +1,12 @@
+// Description:
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// Solution:
+
+function getASCII(c){
+  return c.charCodeAt()
+}
+console.log(getASCII('A'))
+console.log(getASCII(' '))
