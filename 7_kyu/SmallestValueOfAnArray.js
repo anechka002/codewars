cc8f85f63191f2c1dd54090d0626b9d7c9608739
Anna <blinova.anneta@gmail.com>
2025-03-13 10:19:04 +0100

    Smallest value of an array

---
+++ 7_kyu/SmallestValueOfAnArray.js
@@ -0,0 +1,19 @@
+// Description:
+// Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+// Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+// Some examples:
+
+// ([1,2,3,4,5], "value") should return 1
+// ([1,2,3,4,5], "index") should return 0
+
+// Solution:
+
+function min(arr, toReturn) { 
+  let min = Math.min(...arr)
+  let index = arr.indexOf(min)
+  return toReturn === 'value' ? min : index;
+}
+console.log(min([1,2,3,4,5], "value"))
+console.log(min([1,2,3,4,5], "index"))
