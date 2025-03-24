23d8db4292af148eb3ad5cad0d72c7eb464bc7da
Anna <blinova.anneta@gmail.com>
2025-03-24 09:52:06 +0100

    Incorrect division method

---
+++ 8_kyu/IncorrectDivisionMethod.js
@@ -0,0 +1,8 @@
+// Description:
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+// Solution:
+
+const solve = (x, y) => x / y
+
+console.log(solve(0, 8))
