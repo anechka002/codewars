87308d204211d5503cf96a8bd2706512023118a5
Anna <blinova.anneta@gmail.com>
2025-03-27 14:49:15 +0100

    Your order, please

---
+++ 6_kyu/YourOrderPlease.js
@@ -0,0 +1,31 @@
+// Description:
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+// Solution:
+
+function order(words){
+  // const res = words
+  //   .split(' ')
+  //   .sort((a,b) => {
+  //   const a1 = a.match(/\d/)[0]
+  //   const b1 = b.match(/\d/)[0]
+  //   return a1 - b1
+  //   })
+  //   .join(' ');
+  // return res
+
+  // 2 var 
+  return words.split(' ').sort((a, b) => {
+    return a.match(/\d/) - b.match(/\d/);
+  }).join(' ');
+}
+console.log(order("is2 Thi1s T4est 3a")) // "Thi1s is2 3a T4est"
