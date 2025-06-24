b5253e07fa44228d28b8a9b4c048ffd41802dd30
Anna <blinova.anneta@gmail.com>
2025-06-24 14:36:47 +0200

    Merge two sorted arrays into one

---
+++ 8_kyu/Merge-Two-Sorted-Arrays-Into-One.js
@@ -0,0 +1,34 @@
+// DESCRIPTION:
+// You are given two sorted arrays that contain only integers. These arrays may be sorted in either ascending or descending order. Your task is to merge them into a single array, ensuring that:
+
+// The resulting array is sorted in ascending order.
+
+// Any duplicate values are removed, so each integer appears only once.
+
+// If both input arrays are empty, return an empty array.
+
+// No input validation is needed, as both arrays are guaranteed to contain zero or more integers.
+
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+// Happy coding!
+
+function mergeArrays(arr1, arr2) {
+  if(arr1.length === 0 && arr2.length === 0) return [];
+  for (let i = 0; i < arr2.length; i++) {
+    const element = arr2[i];
+    arr1.push(element)
+  }
+  const res = arr1.sort((a,b) => a-b)
+  const unique = new Set(res)
+  const newArr = Array.from(unique)
+  return newArr
+  // return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+}
+
+console.log(mergeArrays([1,2,3,4], [5,6,7,8])) // [1,2,3,4,5,6,7,8]
+console.log(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12])) // [1,2,3,4,5,7,9,10,11,12]
