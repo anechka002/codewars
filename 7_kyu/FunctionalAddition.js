f58b9496a29d7fb42dfb7e97aee4025c174e4a00
Anna <blinova.anneta@gmail.com>
2026-03-27 11:27:03 +0100

    Functional Addition

---
+++ 7_kyu/FunctionalAddition.js
@@ -0,0 +1,18 @@
+// Description:
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// var addOne = add(1);
+// addOne(3); // 4
+
+// var addThree = add(3);
+// addThree(3); // 6
+
+// Solution:
+
+function add(n) {
+  return (a) =>  n + a
+}
+
+console.log(add(3)(1))
